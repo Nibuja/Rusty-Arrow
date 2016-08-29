a539ec11239311010786423e27b70e8a36692433
Nibuja <antoroniii@web.de>
2016-08-29 16:44:15 +0200

    1st Update

---
+++ Rusty Arrow/Rusty Arrow/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRusty
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmRusty))
+        Me.wall_8 = New System.Windows.Forms.PictureBox()
+        Me.wall_5 = New System.Windows.Forms.PictureBox()
+        Me.wall_7 = New System.Windows.Forms.PictureBox()
+        Me.wall_3 = New System.Windows.Forms.PictureBox()
+        Me.wall_2 = New System.Windows.Forms.PictureBox()
+        Me.wall_0 = New System.Windows.Forms.PictureBox()
+        Me.wall_11 = New System.Windows.Forms.PictureBox()
+        Me.wall_10 = New System.Windows.Forms.PictureBox()
+        Me.Player = New System.Windows.Forms.PictureBox()
+        Me.tmrGeneral = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrShootDelay = New System.Windows.Forms.Timer(Me.components)
+        Me.wall_9 = New System.Windows.Forms.PictureBox()
+        Me.wall_4 = New System.Windows.Forms.PictureBox()
+        Me.wall_1 = New System.Windows.Forms.PictureBox()
+        Me.wall_6 = New System.Windows.Forms.PictureBox()
+        Me.gate_0 = New System.Windows.Forms.PictureBox()
+        Me.tmrOpenGate = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrMapChange = New System.Windows.Forms.Timer(Me.components)
+        Me.gate_2 = New System.Windows.Forms.PictureBox()
+        Me.gate_3 = New System.Windows.Forms.PictureBox()
+        Me.gate_1 = New System.Windows.Forms.PictureBox()
+        Me.wall_12 = New System.Windows.Forms.PictureBox()
+        CType(Me.wall_8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gate_0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gate_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gate_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gate_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.wall_12, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'wall_8
+        '
+        Me.wall_8.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_left_bottom_corner
+        resources.ApplyResources(Me.wall_8, "wall_8")
+        Me.wall_8.Name = "wall_8"
+        Me.wall_8.TabStop = False
+        '
+        'wall_5
+        '
+        Me.wall_5.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_right_bottom_corner
+        resources.ApplyResources(Me.wall_5, "wall_5")
+        Me.wall_5.Name = "wall_5"
+        Me.wall_5.TabStop = False
+        '
+        'wall_7
+        '
+        Me.wall_7.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_bottom
+        resources.ApplyResources(Me.wall_7, "wall_7")
+        Me.wall_7.Name = "wall_7"
+        Me.wall_7.TabStop = False
+        '
+        'wall_3
+        '
+        Me.wall_3.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_right
+        resources.ApplyResources(Me.wall_3, "wall_3")
+        Me.wall_3.Name = "wall_3"
+        Me.wall_3.TabStop = False
+        '
+        'wall_2
+        '
+        Me.wall_2.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_right_top_corner
+        resources.ApplyResources(Me.wall_2, "wall_2")
+        Me.wall_2.Name = "wall_2"
+        Me.wall_2.TabStop = False
+        '
+        'wall_0
+        '
+        Me.wall_0.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green
+        resources.ApplyResources(Me.wall_0, "wall_0")
+        Me.wall_0.Name = "wall_0"
+        Me.wall_0.TabStop = False
+        '
+        'wall_11
+        '
+        Me.wall_11.BackColor = System.Drawing.Color.Transparent
+        Me.wall_11.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_left_top_corner
+        resources.ApplyResources(Me.wall_11, "wall_11")
+        Me.wall_11.Name = "wall_11"
+        Me.wall_11.TabStop = False
+        '
+        'wall_10
+        '
+        Me.wall_10.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_left
+        resources.ApplyResources(Me.wall_10, "wall_10")
+        Me.wall_10.Name = "wall_10"
+        Me.wall_10.TabStop = False
+        '
+        'Player
+        '
+        Me.Player.BackColor = System.Drawing.Color.Transparent
+        Me.Player.Image = Global.Rusty_Arrow.My.Resources.Resources.knight_shoot_animation
+        resources.ApplyResources(Me.Player, "Player")
+        Me.Player.Name = "Player"
+        Me.Player.TabStop = False
+        '
+        'tmrGeneral
+        '
+        Me.tmrGeneral.Enabled = True
+        Me.tmrGeneral.Interval = 1
+        '
+        'tmrShootDelay
+        '
+        Me.tmrShootDelay.Interval = 1100
+        '
+        'wall_9
+        '
+        Me.wall_9.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_left
+        resources.ApplyResources(Me.wall_9, "wall_9")
+        Me.wall_9.Name = "wall_9"
+        Me.wall_9.TabStop = False
+        '
+        'wall_4
+        '
+        Me.wall_4.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_right
+        resources.ApplyResources(Me.wall_4, "wall_4")
+        Me.wall_4.Name = "wall_4"
+        Me.wall_4.TabStop = False
+        '
+        'wall_1
+        '
+        Me.wall_1.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green
+        resources.ApplyResources(Me.wall_1, "wall_1")
+        Me.wall_1.Name = "wall_1"
+        Me.wall_1.TabStop = False
+        '
+        'wall_6
+        '
+        Me.wall_6.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_green_bottom
+        resources.ApplyResources(Me.wall_6, "wall_6")
+        Me.wall_6.Name = "wall_6"
+        Me.wall_6.TabStop = False
+        '
+        'gate_0
+        '
+        Me.gate_0.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_wooden_gate_top
+        resources.ApplyResources(Me.gate_0, "gate_0")
+        Me.gate_0.Name = "gate_0"
+        Me.gate_0.TabStop = False
+        '
+        'tmrOpenGate
+        '
+        Me.tmrOpenGate.Interval = 2300
+        '
+        'tmrMapChange
+        '
+        Me.tmrMapChange.Interval = 5
+        '
+        'gate_2
+        '
+        Me.gate_2.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_wooden_gate_bot
+        resources.ApplyResources(Me.gate_2, "gate_2")
+        Me.gate_2.Name = "gate_2"
+        Me.gate_2.TabStop = False
+        '
+        'gate_3
+        '
+        Me.gate_3.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_wooden_gate_left
+        resources.ApplyResources(Me.gate_3, "gate_3")
+        Me.gate_3.Name = "gate_3"
+        Me.gate_3.TabStop = False
+        '
+        'gate_1
+        '
+        Me.gate_1.Image = Global.Rusty_Arrow.My.Resources.Resources.stone_wall_wooden_gate_right
+        resources.ApplyResources(Me.gate_1, "gate_1")
+        Me.gate_1.Name = "gate_1"
+        Me.gate_1.TabStop = False
+        '
+        'wall_12
+        '
+        Me.wall_12.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.Stripes_1
+        resources.ApplyResources(Me.wall_12, "wall_12")
+        Me.wall_12.Name = "wall_12"
+        Me.wall_12.TabStop = False
+        '
+        'frmRusty
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Rusty_Arrow.My.Resources.Resources.stone_floor_green
+        Me.Controls.Add(Me.wall_12)
+        Me.Controls.Add(Me.gate_1)
+        Me.Controls.Add(Me.gate_3)
+        Me.Controls.Add(Me.gate_2)
+        Me.Controls.Add(Me.gate_0)
+        Me.Controls.Add(Me.wall_6)
+        Me.Controls.Add(Me.wall_1)
+        Me.Controls.Add(Me.wall_4)
+        Me.Controls.Add(Me.wall_9)
+        Me.Controls.Add(Me.Player)
+        Me.Controls.Add(Me.wall_8)
+        Me.Controls.Add(Me.wall_5)
+        Me.Controls.Add(Me.wall_7)
+        Me.Controls.Add(Me.wall_3)
+        Me.Controls.Add(Me.wall_2)
+        Me.Controls.Add(Me.wall_0)
+        Me.Controls.Add(Me.wall_11)
+        Me.Controls.Add(Me.wall_10)
+        Me.Name = "frmRusty"
+        CType(Me.wall_8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gate_0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gate_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gate_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gate_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.wall_12, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents wall_10 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_11 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_0 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_7 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_5 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_8 As System.Windows.Forms.PictureBox
+    Friend WithEvents Player As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrGeneral As System.Windows.Forms.Timer
+    Friend WithEvents tmrShootDelay As System.Windows.Forms.Timer
+    Friend WithEvents wall_9 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_4 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_6 As System.Windows.Forms.PictureBox
+    Friend WithEvents gate_0 As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrOpenGate As System.Windows.Forms.Timer
+    Friend WithEvents tmrMapChange As System.Windows.Forms.Timer
+    Friend WithEvents gate_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents gate_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents gate_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents wall_12 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Rusty Arrow/Rusty Arrow/Form1.vb
@@ -0,0 +1,335 @@
+﻿Public Class frmRusty
+    Dim strName, strProj_1, strProj_2, strProj_3 As String
+    Dim animation_val, arrow_count, size_value, map_data(), player_data() As Integer
+    Dim doors As Boolean
+
+    Function random_string() As String
+        Dim strUnique, strTemp, characters As String
+        Dim intRandom As Integer
+        characters = "abcdefghijklmnopjrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        strUnique = ""
+
+        For intIndex As Integer = 0 To 5
+            Randomize()
+            intRandom = CInt(Int((51 * Rnd()) + 1))
+            strTemp = Microsoft.VisualBasic.Left(characters, intRandom)
+            strTemp = Microsoft.VisualBasic.Right(strTemp, 1)
+            strUnique += strTemp
+        Next
+        Return strUnique
+    End Function
+
+    Function AreSameImage(ByVal I1 As Image, ByVal I2 As Image) As Boolean
+        Dim BM1 As Bitmap = I1
+        Dim BM2 As Bitmap = I2
+        For X = 0 To BM1.Width - 1
+            For y = 0 To BM2.Height - 1
+                If BM1.GetPixel(X, y) <> BM2.GetPixel(X, y) Then
+                    Return False
+                End If
+            Next
+        Next
+        Return True
+    End Function
+
+    Function TestIntersection(ByVal name As String, ByVal count As Integer, ByVal player As PictureBox) As Boolean
+        TestIntersection = False
+        For Index As Integer = 0 To count
+            Dim new_name As String = name + CStr(Index)
+            Dim obstacle As PictureBox = CType(Me.Controls(new_name), PictureBox)
+            If player.Bounds.IntersectsWith(obstacle.Bounds) Then
+                Return True
+            End If
+        Next
+    End Function
+
+    'Create a new Timer for each projectile to seperate different types
+    Sub Fly_Mechanism(ByVal direction As Integer, ByVal count As Integer, ByVal speed As Integer, ByVal origin As Point, ByVal image As Image)
+        'Create a universal projectile
+        strName = random_string()
+        Dim projectile_size As Size = image.Size
+        Dim test_projectile As PictureBox
+        test_projectile = New PictureBox
+        test_projectile.Location = origin
+        test_projectile.Image = image
+        test_projectile.Size = projectile_size
+        test_projectile.BackColor = Color.Transparent
+        test_projectile.SizeMode = PictureBoxSizeMode.CenterImage
+        test_projectile.Name = strName
+        Me.Controls.Add(test_projectile)
+        test_projectile.BringToFront()
+
+        'Create a new timer to move the projectile
+        If count = 1 Then
+            Dim tmrTemp_1 As Timer
+            tmrTemp_1 = New Timer
+            tmrTemp_1.Interval = 10
+            tmrTemp_1.Enabled = True
+            AddHandler tmrTemp_1.Tick, AddressOf Me.tmrTemp_1_tick
+            strProj_1 = strName
+        ElseIf count = 2 Then
+            Dim tmrTemp_2 As Timer
+            tmrTemp_2 = New Timer
+            tmrTemp_2.Interval = 10
+            tmrTemp_2.Enabled = True
+            AddHandler tmrTemp_2.Tick, AddressOf Me.tmrTemp_2_tick
+            strProj_2 = strName
+        ElseIf count = 3 Then
+            Dim tmrTemp_3 As Timer
+            tmrTemp_3 = New Timer
+            tmrTemp_3.Interval = 10
+            tmrTemp_3.Enabled = True
+            AddHandler tmrTemp_3.Tick, AddressOf Me.tmrTemp_3_tick
+            strProj_3 = strName
+        End If
+    End Sub
+
+    Friend WithEvents tmrTemp_1 As System.Windows.Forms.Timer
+    Private Sub tmrTemp_1_tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTemp_1.Tick
+        Dim projectile As PictureBox = CType(Me.Controls(strProj_1), PictureBox)
+        Dim Loc As Point
+        Loc = New Point(projectile.Location.X + 2, projectile.Location.Y)
+        projectile.Location = Loc
+    End Sub
+
+    Friend WithEvents tmrTemp_2 As System.Windows.Forms.Timer
+    Private Sub tmrTemp_2_tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTemp_2.Tick
+        Dim projectile As PictureBox = CType(Me.Controls(strProj_2), PictureBox)
+        Dim Loc As Point
+        Loc = New Point(projectile.Location.X + 2, projectile.Location.Y)
+        projectile.Location = Loc
+    End Sub
+
+    Friend WithEvents tmrTemp_3 As System.Windows.Forms.Timer
+    Private Sub tmrTemp_3_tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTemp_3.Tick
+        Dim projectile As PictureBox = CType(Me.Controls(strProj_3), PictureBox)
+        Dim Loc As Point
+        Loc = New Point(projectile.Location.X + 2, projectile.Location.Y)
+        projectile.Location = Loc
+    End Sub
+
+    Sub MapChange(ByVal something As String)
+        'Coming Soon
+        'All Map Change and creation is done here
+        MsgBox(something)
+        size_value += 1
+        tmrMapChange.Enabled = True
+        If map_data(0) > 0 Then
+
+
+
+    End Sub
+
+    Sub MapCreation()
+        If map_data(0) > 0 Then
+
+
+
+
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub frmRusty_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+
+        '/Move the Player with WASD
+        Dim Loc As Point
+        Dim imgAni As Image = My.Resources.knight_shoot_animation
+        If e.KeyCode = Keys.A Or e.KeyCode = Keys.D Or e.KeyCode = Keys.S Or e.KeyCode = Keys.W Then
+            If Not (animation_val = 2) Then
+                animation_val = 1
+            End If
+        End If
+
+        If Not (tmrShootDelay.Enabled = True) Then
+
+            If e.KeyCode = Keys.A Then
+                Loc = New Point(Player.Location.X - 3, Player.Location.Y)
+                Player.Location = Loc
+            ElseIf e.KeyCode = Keys.D Then
+                Loc = New Point(Player.Location.X + 3, Player.Location.Y)
+                Player.Location = Loc
+            ElseIf e.KeyCode = Keys.S Then
+                Loc = New Point(Player.Location.X, Player.Location.Y + 3)
+                Player.Location = Loc
+            ElseIf e.KeyCode = Keys.W Then
+                Loc = New Point(Player.Location.X, Player.Location.Y - 3)
+                Player.Location = Loc
+            End If
+            If e.KeyCode = Keys.Space Then
+                animation_val = 2
+                tmrGeneral.Enabled = False
+                tmrGeneral.Enabled = True
+                tmrGeneral.Interval = 1
+                tmrShootDelay.Enabled = True
+            End If
+
+            If TestIntersection("wall_", 11, Player) = True Then
+                Player.Location = New Point(200, 200)
+            End If
+
+            If Player.Bounds.IntersectsWith(gate_0.Bounds) And doors = True And tmrOpenGate.Enabled = False Then
+                gate_0.Image = My.Resources.stone_wall_wooden_gate_open
+                tmrOpenGate.Enabled = True
+            End If
+
+            Player.BringToFront()
+
+        End If
+
+    End Sub
+
+    Private Sub frmRusty_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        If (e.KeyCode = Keys.A Or e.KeyCode = Keys.D Or e.KeyCode = Keys.S Or e.KeyCode = Keys.W) And Not (tmrGeneral.Interval = 1450) Then
+            tmrGeneral.Interval = 1
+            animation_val = 0
+            tmrGeneral.Enabled = False
+            tmrGeneral.Enabled = True
+        ElseIf e.KeyCode = Keys.Space Then
+            tmrGeneral.Interval = 1450
+            animation_val = 0
+            tmrGeneral.Enabled = False
+            tmrGeneral.Enabled = True
+        End If
+    End Sub
+
+
+    Private Sub tmrGeneral_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrGeneral.Tick
+
+        If animation_val = 0 Then
+            Player.Image = My.Resources.knight_basic_2
+            tmrGeneral.Interval = 1
+        ElseIf animation_val = 1 Then
+            Player.Image = My.Resources.knight_walk_animation
+            tmrGeneral.Interval = 1000
+        ElseIf animation_val = 2 Then
+            Player.Image = My.Resources.knight_shoot_animation
+            tmrGeneral.Interval = 1450
+        End If
+
+    End Sub
+
+    Private Sub Player_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Player.Click
+        'MsgBox(random_string())
+        doors = True
+        gate_0.Image = My.Resources.stone_wall_wooden_gate_top
+
+        'Test-Start for Map Change
+        Dim count, location_value, rest As Integer
+        count = 0
+        location_value = 70
+        For Index As Integer = 0 To 10
+            Dim stripe As PictureBox = New PictureBox
+            Dim box_location As Point
+            rest = Index Mod 2
+            If rest = 1 Then
+                box_location = New Point(location_value * Index, 580)
+            Else
+                box_location = New Point(location_value * Index, 0)
+            End If
+            Dim name As String = "black_box_" + CStr(Index)
+            stripe.Location = box_location
+            stripe.BackColor = Color.Black
+            stripe.Size = New Size(location_value, 0)
+            stripe.Name = name
+            Me.Controls.Add(stripe)
+            stripe.BringToFront()
+        Next
+        tmrMapChange.Enabled = True
+        size_value = 0
+    End Sub
+
+
+    Private Sub tmrShootDelay_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrShootDelay.Tick
+        If arrow_count = 4 Then
+            arrow_count = 1
+        End If
+        Fly_Mechanism(2, arrow_count, 2, (New Point(Player.Location.X + 15, Player.Location.Y + 19)), (My.Resources.arrow_basic_fly_animation))
+        tmrShootDelay.Enabled = False
+        arrow_count += 1
+    End Sub
+
+    Private Sub frmRusty_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        arrow_count = 1
+        doors = True
+        ReDim player_data(5)
+        'Alive(f=0/t=1); Direction(t=0,r=1,b=2,l=3); Level(0-30); Health(0-100%), Mana(0-100%); StatusEffects(0-6)
+        player_data(0) = 1
+        player_data(1) = 0
+        player_data(2) = 1
+        player_data(3) = 100
+        player_data(4) = 100
+        player_data(5) = 0
+
+
+        ReDim map_data(9)
+        'Floor(0-X); MapType(0-X); MapStyle(0-X); Visited(f=0/t=1); Doors(0-4); Entrence(0-4); Objects(0-X); Enemies(f=0/t=1); EnemyCount(0-X); EnemyType(0-X)
+        'MapType(0 = Empty; 1 = Quest; 2 = Treasure; 3 = Fighting; 4 = Arena; 5 = Boss; 6 = Other)
+        map_data(0) = 1
+        map_data(1) = 0
+        map_data(2) = 1
+        map_data(3) = 0
+        map_data(4) = 4
+        map_data(5) = 1
+        map_data(6) = 0
+        map_data(7) = 0
+        map_data(8) = 0
+        map_data(9) = 0
+
+
+    End Sub
+
+    Private Sub tmrOpenGate_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrOpenGate.Tick
+        tmrOpenGate.Enabled = False
+        gate_0.Image = My.Resources.stone_wall_wooden_gate_top_6
+        doors = False
+    End Sub
+
+    Private Sub tmrMapChange_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMapChange.Tick
+        Dim rest As Integer
+        If size_value <= 580 Then
+            For Index As Integer = 0 To 10
+                Dim name As String = "black_box_" + CStr(Index)
+                Dim black_box As PictureBox = CType(Me.Controls(name), PictureBox)
+                Dim size As Size = New Size(black_box.Size.Width, size_value)
+                Dim location As Point = New Point(black_box.Location.X, black_box.Location.Y - 7)
+                rest = Index Mod 2
+                If rest = 1 Then
+                    black_box.Location = location
+                End If
+                black_box.Size = size
+            Next
+            size_value += 7
+        ElseIf size_value = 581 Then
+            tmrMapChange.Enabled = False
+            MapChange("something is done here!")
+        ElseIf size_value > 581 And size_value <= 1160 Then
+            For Index As Integer = 0 To 10
+                Dim name As String = "black_box_" + CStr(Index)
+                Dim black_box As PictureBox = CType(Me.Controls(name), PictureBox)
+                Dim size As Size = New Size(black_box.Size.Width, 1160 - size_value)
+                Dim location_1 As Point = New Point(black_box.Location.X, 0)
+                Dim location_2 As Point = New Point(black_box.Location.X, black_box.Location.Y + 7)
+                rest = Index Mod 2
+                If rest = 1 Then
+                    black_box.Location = location_1
+                Else
+                    black_box.Location = location_2
+                End If
+                black_box.Size = size
+            Next
+            size_value += 7
+        Else
+            tmrMapChange.Enabled = False
+            size_value = 0
+            For Index As Integer = 0 To 10
+                Dim name As String = "black_box_" + CStr(Index)
+                Dim black_box As PictureBox = CType(Me.Controls(name), PictureBox)
+                Me.Controls.Remove(black_box)
+            Next
+        End If
+    End Sub
+End Class
