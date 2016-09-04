ad9619bccfe6bce10e3824c8a42ffd0e18d502e3
Nibuja <antoroniii@web.de>
2016-09-04 13:58:01 +0200

    FIrst Release
    Test Project Map_Generation
    -> generates a whole map with chooseable size and special rooms, etc.

---
+++ Map_Generation/Map_Generation/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGeneration
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnGenerator = New System.Windows.Forms.Button()
+        Me.room_0 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.room_0, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGenerator
+        '
+        Me.btnGenerator.Location = New System.Drawing.Point(147, 23)
+        Me.btnGenerator.Name = "btnGenerator"
+        Me.btnGenerator.Size = New System.Drawing.Size(269, 85)
+        Me.btnGenerator.TabIndex = 0
+        Me.btnGenerator.Text = "Generate!"
+        Me.btnGenerator.UseVisualStyleBackColor = True
+        '
+        'room_0
+        '
+        Me.room_0.BackColor = System.Drawing.Color.OrangeRed
+        Me.room_0.Location = New System.Drawing.Point(279, 293)
+        Me.room_0.Name = "room_0"
+        Me.room_0.Size = New System.Drawing.Size(20, 20)
+        Me.room_0.TabIndex = 1
+        Me.room_0.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(450, 56)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'frmGeneration
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(580, 533)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.room_0)
+        Me.Controls.Add(Me.btnGenerator)
+        Me.KeyPreview = True
+        Me.Name = "frmGeneration"
+        Me.Text = "Map Generator"
+        CType(Me.room_0, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGenerator As System.Windows.Forms.Button
+    Friend WithEvents room_0 As System.Windows.Forms.PictureBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Map_Generation/Map_Generation/Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class frmGeneration
+    'MAP GENERATION FORM
+    'Test Project for Map Generation (Rusty Arrow)
+    'AUTHOR: Anton Winkler
+    'DATE: 04.09.2016 13:51 MEZ
+
+    Dim Room_Nr, room_size As Integer
+
+    'Creates a new Location in given direction of the given point
+    Function New_Location(ByVal direction As Integer, ByVal old_loc As Point) As Point
+        New_Location = old_loc
+        Select Case direction
+            Case 0
+                New_Location.X = old_loc.X - 21
+            Case 1
+                New_Location.Y = old_loc.Y + 21
+            Case 2
+                New_Location.X = old_loc.X + 21
+            Case 3
+                New_Location.Y = old_loc.Y - 21
+        End Select
+    End Function
+
+    'Tests if there is already another room in given direction of the given point
+    Function Search_Rooms(ByVal direction As Integer, ByVal old_loc As Point) As Boolean
+        Dim test As Boolean = False
+        Dim Test_box As PictureBox = New PictureBox
+        Test_box.Size = New Size(2, 2)
+        Test_box.Location = New_Location(direction, old_loc)
+        Me.Controls.Add(Test_box)
+        Dim Next_room As PictureBox
+        Dim next_name As String
+        Dim Index As Integer = 0
+        Do
+            Dim new_name As String = "room_" + CStr(Index)
+            Dim Test_Room As PictureBox = CType(Me.Controls(new_name), PictureBox)
+            If Test_box.Bounds.IntersectsWith(Test_Room.Bounds) Then
+                test = True
+            End If
+            next_name = "room_" + CStr(Index + 1)
+            Next_room = CType(Me.Controls(next_name), PictureBox)
+            Index += 1
+        Loop Until (Next_room Is Nothing)
+        Me.Controls.Remove(Test_box)
+        Return test
+    End Function
+
+    'Tests if the start room is a neightbour room
+    Function Search_Start_Room(ByVal new_room As PictureBox, ByVal start_room As PictureBox) As Boolean
+        Dim test As Boolean = False
+        For Index As Integer = 0 To 3
+            Dim Test_box As PictureBox = New PictureBox
+            Test_box.Size = New Size(2, 2)
+            Test_box.Location = New_Location(Index, new_room.Location)
+            Me.Controls.Add(Test_box)
+            If Test_box.Bounds.IntersectsWith(start_room.Bounds) Then
+                test = True
+            End If
+            Me.Controls.Remove(Test_box)
+        Next
+        Return test
+    End Function
+
+    'Test for same special rooms
+    Function Same_Room(ByVal test_room As PictureBox, ByVal rooms() As Integer) As Boolean
+        Dim test As Boolean = False
+        For Each number In rooms
+            Dim new_name As String = "room_" + CStr(number)
+            Dim new_room As PictureBox = CType(Me.Controls(new_name), PictureBox)
+            If new_room.Bounds.IntersectsWith(test_room.Bounds) Then
+                test = True
+            End If
+        Next
+        Return test
+    End Function
+
+    'Main Sub for generating the map, room by room; repeats itself with decreasing chance of new connections
+    Sub Create_Map(ByVal chance As Integer, ByVal last_room As PictureBox, ByVal direction As Integer)
+        Dim new_chance As Integer
+        Dim directions As Integer() = {0, 0, 0, 0}
+        Dim new_room As PictureBox = New PictureBox
+        new_room.Size = New Size(20, 20)
+        new_room.Location = New_Location(direction, last_room.Location)
+        new_room.BackColor = Color.DarkRed
+        new_room.Name = "room_" + CStr(Room_Nr)
+        Me.Controls.Add(new_room)
+        Room_Nr += 1
+        For Index_1 As Integer = 0 To 3
+            Randomize()
+            new_chance = CInt(Int((100 * Rnd()) + (50 - chance)))
+            chance += room_size
+            If new_chance > 50 Then
+                Dim tester As Boolean = False
+                Do
+                    Randomize()
+                    Dim direction_chance As Integer = CInt(Int((4 * Rnd()) + 1))
+                    If Not (directions(direction_chance - 1) = 1) Then
+                        directions(direction_chance - 1) = 1
+                        tester = True
+                        If Search_Rooms(direction_chance - 1, new_room.Location) = False Then
+                            Create_Map(chance, new_room, direction_chance - 1)
+                        End If
+                    End If
+                Loop Until tester = True
+            End If
+        Next
+    End Sub
+
+    'Deletes all old rooms
+    Sub Delete_all()
+        Dim next_name As String
+        Dim next_room As PictureBox
+        Dim Index As Integer
+        Do
+            Dim new_name As String = "room_" + CStr(Index)
+            Dim Test_Room As PictureBox = CType(Me.Controls(new_name), PictureBox)
+            Me.Controls.Remove(Test_Room)
+            next_name = "room_" + CStr(Index + 1)
+            next_room = CType(Me.Controls(next_name), PictureBox)
+            Index += 1
+        Loop Until (next_room Is Nothing)
+    End Sub
+
+    'Generates a random Color
+    Function Random_Color() As Color
+        Dim MyAlpha As Integer
+        Dim MyRed As Integer
+        Dim MyGreen As Integer
+        Dim MyBlue As Integer
+        MyAlpha = 254
+        Randomize()
+        MyRed = CInt(Int((254 * Rnd()) + 0))
+        Randomize()
+        MyGreen = CInt(Int((254 * Rnd()) + 0))
+        Randomize()
+        MyBlue = CInt(Int((254 * Rnd()) + 0))
+
+        Random_Color = Color.FromArgb(MyAlpha, MyRed, MyGreen, MyBlue)
+    End Function
+
+    Private Sub btnGenerator_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGenerator.Click
+        'Check, if there is a wrong input
+        If TextBox1.Text = Nothing Or TextBox1.Text = "0" Then
+            TextBox1.Text = "5"
+        End If
+        'Reset to standard
+        room_size = CInt(TextBox1.Text)
+        Room_Nr = 0
+        Delete_all()
+        'Start the generation from every side
+        Create_Map(0, room_0, 0)
+        Create_Map(0, room_0, 1)
+        Create_Map(0, room_0, 2)
+        Create_Map(0, room_0, 3)
+        'Make the start room visible (by creating a new one on same place)
+        room_0.BringToFront()
+        Dim start_room As PictureBox = New PictureBox
+        start_room.Location = room_0.Location
+        start_room.BackColor = Color.PaleVioletRed
+        start_room.Size = New Size(20, 20)
+        start_room.Name = "start_room"
+        Me.Controls.Add(start_room)
+        start_room.BringToFront()
+        btnGenerator.Text = CStr(Room_Nr)
+        'After generation create random special rooms
+        Dim special_rooms As Integer() = {0, 0, 0, 0, 0}
+        Dim test As Boolean = True
+        Dim random, Index As Integer
+        Dim special_room As PictureBox
+        Dim special_name As String
+        Index = 0
+        Do
+            Randomize()
+            'Create a random number and search the corresponding room
+            random = CInt(Int((Room_Nr - 1) * Rnd()) + 1)
+            special_name = "room_" + CStr(random)
+            special_room = CType(Me.Controls(special_name), PictureBox)
+            'Check if the room is already special or next to the start room
+            If Search_Start_Room(special_room, start_room) = False And Not (special_room.Bounds.IntersectsWith(start_room.Bounds)) Then
+                If Same_Room(special_room, special_rooms) = False Then
+                    special_rooms(Index) = random
+                    special_room.BackColor = Color.DarkSalmon
+                    special_room.BringToFront()
+                    Index += 1
+                End If
+            End If
+        Loop Until Index = 5
+        'Just a test for the save of the special rooms
+        Label1.Text = CStr(special_rooms(0)) + "," + CStr(special_rooms(1)) + "," + CStr(special_rooms(2)) + "," + CStr(special_rooms(3)) + "," + CStr(special_rooms(4))
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        'btnGenerator.BackColor = Random_Color()
+
+
+    End Sub
+End Class
